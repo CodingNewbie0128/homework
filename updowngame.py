f1272abd516362c4219ec8480d0c7598b3f08660
CodingNewbie0128 <engn0383@naver.com>
2024-06-29 00:23:50 +0900

    updowngame ì완성버전

--- updowngame.py
+++ updowngame.py
@@ -1,4 +1,43 @@
 import random
 
-print("asd")
+
+def setNum():
+    num=random.randint(1,100)
+    return num
+def game():
+    count=1
+    num=setNum()
+
+    while True:
+        user_num = int(input("숫자를 입력하세요 :"))
+        if user_num > 100 or user_num < 1:
+            print("유효한 범위 내의 숫자를 입력하세요")
+            game()
+        if num==user_num:
+            print("맞았습니다\n시도한 횟수 :{}".format(count))
+            decision=input("다시 하겠습니까? (y/n) ")
+            if decision=="y":
+                game()
+
+            if decision=="n":
+                break;
+        elif num<user_num:
+            print("down")
+            count += 1
+        else :
+            print("up")
+            count += 1
+
+    print("게임을 종료합니다")
+
+
+
+game()
+
+
+
+
+
+
+
 
